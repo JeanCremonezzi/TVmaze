506cf93dd1873b68983654329f3956f2861f366b
JeanCremonezzi <jeancremonezzi@hotmail.com>
2021-11-25 16:52:05 -0300

    feat: Card component

--- events.js
+++ events.js
@@ -10,6 +10,7 @@ const getShows = (querySearch) => {
         method: "GET",
         url: ` https://api.tvmaze.com/search/shows?q=${querySearch}`,
         success: (resp) => {
+            console.log(resp);
             populateShows(resp);
             return;
         },
@@ -20,5 +21,36 @@ const getShows = (querySearch) => {
 };
 
 const populateShows = (shows) => {
-    console.log(shows);
+    shows.map((show) => {
+        let showCard = componentCard(show.show);
+
+        $(".container").append(showCard);
+    });
 };
+
+const componentCard = (show) => {
+    let col = $("<div></div>");
+    col.attr({"class": "col"});
+
+    let card = $("<div></div>");
+    card.attr({"class": "card"});
+    let imgUrl = show.image;
+
+    if (imgUrl != null) {
+        card.css("background-image", `url('${imgUrl.medium}')`);
+    };
+
+    let layer = $("<div></div>");
+    layer.attr({"class": "cardLayer"});
+    
+    let name = $(`<p>${show.name}</p>`);
+    name.attr({"class": "cardTitle"});
+
+    let summary = $(`<p>${show.summary}</p>`);
+    summary.attr({"class": "cardSummary"});
+
+    layer.append(name, summary);
+    col.append(card, layer);
+
+    return col;
+}
