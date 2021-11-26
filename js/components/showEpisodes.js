17997553b850ae02e97231cd29e1f879519f7ec6
JeanCremonezzi <jeancremonezzi@hotmail.com>
2021-11-26 11:16:43 -0300

    feat: Show episodes info

--- js/components/showEpisodes.js
+++ js/components/showEpisodes.js
@@ -11,9 +11,12 @@ export function getEpisodes (show) {
 const modalEpisodes = (name, episodes) => {
     $("body").css("overflow", "hidden");
 
-    let modal = $(`<div>${name}</div>`);
+    let modal = $("<div></div>");
     modal.attr({"class": "modal"});
 
+    let showName = $(`<h1>${name}</h1>`);
+    modal.append(showName);
+    
     let overlay = $("<div></div>");
     overlay.attr({"class": "overlay"});
 
@@ -22,6 +25,46 @@ const modalEpisodes = (name, episodes) => {
         $(".modal").remove();
         $("body").css("overflow", "auto");
     });
+
+    episodes.map((episode) => {
+        modal.append(rowEpisode(episode));
+    });
     
     $("body").prepend(overlay, modal);
+};
+
+const rowEpisode = (episode) => {
+    console.log(episode);
+    let row = $("<div></div>");
+
+    let urlImg = "";
+    if (episode.image != null && episode.image.medium != "") {
+        urlImg = episode.image.medium;
+    };
+    let img = $("<img>");
+    img.attr({"src": urlImg});
+    row.attr({"class": "episodeRow"});
+
+    let info = $("<div></div>");
+    info.attr({"class": "episodeInfo"});
+
+    let name = $(`<span>${episode.name}</span>`);
+
+    let season = $(`<small>S${episode.season} E${episode.number}</small>`);
+
+    let dataExibicao = $(`<small>Exibido em ${formatDate(episode.airdate)}</small>`);
+
+    info.append(name, season, dataExibicao);
+    row.append(img, info);
+
+    return row;
+};
+
+const formatDate = (date) => {
+    date = new Date(date);
+
+    const timezoneOffset = date.getTimezoneOffset();
+    date.setTime(date.getTime() + (timezoneOffset * 60000));
+
+    return(date.toLocaleDateString("pt-BR"));
 };
